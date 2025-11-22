7e80774ef8abd59fcfef3bac3658421c93f85a09
fifovalle <fifanaufal10@gmail.com>
2025-11-22 15:42:01 +0700

    feat: instalasi

---
+++ src/main.ts
@@ -0,0 +1,36 @@
+import { join } from 'path';
+import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
+import { apiReference } from '@scalar/nestjs-api-reference';
+import { SwaggerModule, OpenAPIObject } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const document: OpenAPIObject = SwaggerModule.createDocument(app, {
+    openapi: '3.1.0',
+    info: { title: 'MET Flight Radar API', version: '1.0.0' },
+  });
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+  app.use('/openapi.json', (req, res) => res.json(document));
+
+  app.use('/docs', apiReference({ url: '/openapi.json', theme: 'default' }));
+
+  app.use((req, res, next) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+    res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    next();
+  });
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+
+  const port = process.env.PORT ? +process.env.PORT : 3000;
+  await app.listen(port);
+  console.log(`🚀 Localhost ready: http://localhost:${port}`);
+  console.log(`📄 Docs: http://localhost:${port}/docs`);
+}
+
+void bootstrap();
