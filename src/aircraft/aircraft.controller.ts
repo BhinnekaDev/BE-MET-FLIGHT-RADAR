911989fc53d0a4d4a7d4f66d0fc00d60ac4f734a
marco-sihombing <injapri@gmail.com>
2025-11-26 07:06:06 +0700

    feat: prediksi cuaca esok hari

--- src/aircraft/aircraft.controller.ts
+++ src/aircraft/aircraft.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AircraftService } from './aircraft.service';
 
 @Controller('aircraft')
 export class AircraftController {
   constructor(private readonly aircraftService: AircraftService) {}
 
-  @Get('fetch')
-  async fetch() {
-    return this.aircraftService.fetchAircraftData();
-  }
+  // @Get('fetch')
+  // async fetch() {
+  //   return this.aircraftService.fetchAircraftData();
+  // }
 }
